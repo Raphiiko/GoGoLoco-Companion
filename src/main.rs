945b1686a0869cad648e79332a6768829e101ce6
Raphii <iam@raphii.co>
2022-09-29 14:15:57 +0200

    Fix release of thumb stick

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn main() {
     };
     let sock = UdpSocket::bind(addr).unwrap();
     println!("Connected to VRChat");
-    
+
     // Listen for OSC messages
     let mut buf = [0u8; rosc::decoder::MTU];
     let mut last_vrc_emote: i32 = 0;
@@ -76,9 +76,9 @@ fn handle_osc_packet(
                     rosc::OscType::Int(value) => value,
                     _ => -1,
                 };
-                println!("VRCEmote {}", vrc_emote);
                 if *last_vrc_emote == 212 && vrc_emote == 0 {
                     *last_vrc_emote = vrc_emote;
+                    println!("Scaling Radial Menu Closed");
                     if focus("VRChat") {
                         osc_toggle_sequence(gamepad, controller);
                     }
@@ -100,11 +100,16 @@ fn osc_toggle_sequence(gamepad: &mut XGamepad, controller: &mut Xbox360Wired<Cli
     for _ in 0..2 {
         click_angle(gamepad, controller, 180.0);
     }
+    // Reset thumb stick
+    gamepad.thumb_rx = 0;
+    gamepad.thumb_ry = 0;
+    let _ = controller.update(&gamepad);
     println!("OSC Toggle Sequence Complete");
 }
 
 fn click_angle(gamepad: &mut XGamepad, controller: &mut Xbox360Wired<Client>, angle: f64) {
     println!("Clicking Menu option at {} degrees", angle);
+    // Set thumb stick angle if needed
     let x = (angle.to_radians().sin() * 30000.0) as i16;
     let y = (angle.to_radians().cos() * 30000.0) as i16;
     if x != gamepad.thumb_rx || y != gamepad.thumb_ry {
@@ -113,9 +118,11 @@ fn click_angle(gamepad: &mut XGamepad, controller: &mut Xbox360Wired<Client>, an
         let _ = controller.update(&gamepad);
         std::thread::sleep(Duration::from_millis(200));
     }
+    // Press trigger
     gamepad.right_trigger = 255;
     let _ = controller.update(&gamepad);
     std::thread::sleep(Duration::from_millis(100));
+    // Release trigger
     gamepad.right_trigger = 0;
     let _ = controller.update(&gamepad);
     std::thread::sleep(Duration::from_millis(300));
